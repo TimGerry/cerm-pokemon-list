b1b4fdf9be785447883692be7f851c56d163630a
TimGe <timgerry.geerts@infosupport.com>
2024-03-11 14:04:38 +0100

    Template-driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Pokemon } from './models/pokemon.model';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import { PokemonTypePipe } from './pipes/pokemon-type.pipe';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CurrencyPipe, DatePipe, PokemonTypePipe],
+  imports: [RouterOutlet, CurrencyPipe, DatePipe, PokemonTypePipe, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -16,9 +17,24 @@ export class AppComponent {
   today = new Date();
   pokemonList: Pokemon[] = [
     { id: 'bulbasaur', name: 'bulbasaur', type: 'grass', type2: 'poison', attack: 'razor leaf', level: 5 },
-    { id: 'bulbasaur',  name: 'squirtle', type: 'water', attack: 'water gun', level: 6 },
+    { id: 'bulbasaur', name: 'squirtle', type: 'water', attack: 'water gun', level: 6 },
     { id: 'bulbasaur', name: 'charmander', type: 'fire', attack: 'ember', level: 5 }
   ];
+
+  pokemonForm: Pokemon = {
+    id: '',
+    name: '',
+    type: '',
+    type2: '',
+    attack: '',
+    level: 5
+  }
+
+  submit(form: NgForm) {
+    const pokemonToAdd = { ...this.pokemonForm, id: this.pokemonForm.name };
+    this.pokemonList.push(pokemonToAdd);
+    form.reset();
+  }
 
   getPokemonImage(name: string) {
     return `https://img.pokemondb.net/artwork/avif/${name}.avif`
