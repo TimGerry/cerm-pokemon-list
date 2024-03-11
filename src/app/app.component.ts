889e82cbfb797df8b687348f37d1a0cad7512763
TimGe <timgerry.geerts@infosupport.com>
2024-03-11 11:25:21 +0100

    Core Typescript

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Pokemon } from './models/pokemon.model';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +11,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Pokémon List';
-  pokemonList = [
-    { name: 'bulbasaur', type: 'grass', type2: 'poison', attack: 'razor leaf', level: 5 },
-    { name: 'squirtle', type: 'water',  attack: 'water gun', level: 6 },
-    { name: 'charmander', type: 'fire', attack: 'ember', level: 5 }
+  pokemonList: Pokemon[] = [
+    { id: 'bulbasaur', name: 'bulbasaur', type: 'grass', type2: 'poison', attack: 'razor leaf', level: 5 },
+    { id: 'bulbasaur',  name: 'squirtle', type: 'water', attack: 'water gun', level: 6 },
+    { id: 'bulbasaur', name: 'charmander', type: 'fire', attack: 'ember', level: 5 }
   ];
 
   getPokemonImage(name: string) {
     return `https://img.pokemondb.net/artwork/avif/${name}.avif`
   }
 
-  attack(pokemon: any) {
+  attack(pokemon: Pokemon) {
     window.alert(`${pokemon.name} used ${pokemon.attack}!`);
   }
 }
---
+++ src/app/models/pokemon.model.ts
@@ -0,0 +1,8 @@
+export interface Pokemon {
+  id: string;
+  name: string;
+  type: string;
+  type2?: string;
+  attack: string;
+  level: number;
+}
