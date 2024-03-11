b14835ac03935eb13e37c40a5fb24ec95acec221
TimGe <timgerry.geerts@infosupport.com>
2024-03-11 13:26:40 +0100

    Unit test

--- src/app/pipes/pokemon-type.pipe.spec.ts
+++ src/app/pipes/pokemon-type.pipe.spec.ts
@@ -1,8 +1,33 @@
+import e from 'express';
 import { PokemonTypePipe } from './pokemon-type.pipe';
 
-describe('PokemonTypePipe', () => {
+fdescribe('PokemonTypePipe', () => {
+  let sut: PokemonTypePipe;
+
+  beforeEach(() => {
+    sut = new PokemonTypePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new PokemonTypePipe();
-    expect(pipe).toBeTruthy();
+    expect(sut).toBeTruthy();
+  });
+
+  it('should return type', () => {
+      const expected = 'water';
+
+      let actual = sut.transform(expected);
+
+      expect(actual).toEqual(expected);
+  });
+
+  it('should return custom message', () => {
+    let expected = '-';
+    let actual = sut.transform(undefined, expected);
+    expect(actual).toEqual(expected);
+  });
+
+  it('should return standard message', () => {
+    let actual = sut.transform(undefined);
+    expect(actual).toEqual('Unavailable');
   });
 });
